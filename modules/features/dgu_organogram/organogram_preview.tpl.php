e19470d9d9eeeae9578f733d533e1deb9190a19e
Noel Sharpe <noels@radnetwork.co.uk>
2014-06-30 08:50:08 +0100

    Organogram code now in own module.

--- modules/features/dgu_organogram/organogram_preview.tpl.php
+++ modules/features/dgu_organogram/organogram_preview.tpl.php
@@ -2,7 +2,6 @@
   <div class="organogram-preview__inner">
     <!--TODO: change me into a button with no styling, or an input[type=button] so you can tab to me and i have the right cursor -->
     <div class="organogram-preview-close">&times;</div>
-
     <!-- TODO prettier labeling -->
     <label>
         <input type="radio" class="organogram-button" name="optionsRadios" value="option1" checked="checked">
