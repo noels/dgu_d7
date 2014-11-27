14fc7f6cdd3730939ed42690d1acc22e8d28584f
Noel Sharpe <noels@radnetwork.co.uk>
2014-11-27 12:45:16 +0000

    Theme consultation index output.

---
+++ modules/features/dgu_consultation/templates/consultation-index.tpl.php
@@ -0,0 +1,12 @@
+<h2> <dt><?php print $consultation_index['section']; ?></dt><dd><?php print $consultation_index['title']; ?></dd></h2>
+<ul>
+  <?php foreach ($consultation_index['paragraphs'] as $paragraph): ?>
+  <li><dt><?php print $paragraph['section'] ?></dt><dd><?php print $paragraph['title'] ?></dd></li>
+  <?php endforeach; ?>
+
+  <?php if (!empty($consultation_index['subsections'])): ?>
+    <?php foreach ($consultation_index['subsections'] as $subsection): ?>
+      <li><?php print(theme('consultation_index', array('consultation_index' => $subsection))); ?></li>
+    <?php endforeach; ?>
+  <?php endif; ?>
+</ul>
