f3fd675241963d536ad5802936019d11c21e91f1
Noel Sharpe <noels@radnetwork.co.uk>
2014-12-01 12:46:27 +0000

    dd and dt elements should only occur in a dl element.

--- modules/features/dgu_consultation/templates/consultation-index.tpl.php
+++ modules/features/dgu_consultation/templates/consultation-index.tpl.php
@@ -1,7 +1,7 @@
-<h4><a href="<?php print($consultation_index['href']); ?>"<dt><?php print $consultation_index['section']; ?></dt><dd><?php print $consultation_index['title']; ?></dd></a></h4>
+<h4><a href="<?php print($consultation_index['href']); ?>"><?php print $consultation_index['section']; ?> <?php print $consultation_index['title']; ?></a></h4>
 <ul>
   <?php foreach ($consultation_index['paragraphs'] as $paragraph): ?>
-  <li><dt><?php print $paragraph['section']; ?></dt><dd><?php print $paragraph['title'] ?></dd></li>
+  <li><?php print $paragraph['section']; ?> <?php print $paragraph['title'] ?></li>
   <?php endforeach; ?>
 
   <?php if (!empty($consultation_index['subsections'])): ?>
