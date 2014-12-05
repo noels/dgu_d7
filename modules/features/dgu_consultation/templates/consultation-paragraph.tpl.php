aec45feefe7fd981dd20edc75e991bffc2f51598
Noel Sharpe <noels@radnetwork.co.uk>
2014-12-05 13:37:25 +0000

    Move themeing to template and provide more structure for styling.

--- modules/features/dgu_consultation/templates/consultation-paragraph.tpl.php
+++ modules/features/dgu_consultation/templates/consultation-paragraph.tpl.php
@@ -5,9 +5,12 @@
   <div class="field-items"<?php print $content_attributes; ?>>
     <?php foreach ($items as $delta => $item): ?>
       <div class="field-item <?php print $delta % 2 ? 'odd' : 'even'; ?>"<?php print $item_attributes[$delta]; ?>>
-        <div class="consultation-paragraph"><?php print render($item['field']); ?></div>
-        <?php print render($item['replies']) ?>
-        <?php print render($item['links']) ?>
+        <div class="consultation-paragraph">
+          <span class="subsection"><a id="<?php print $item['section']; ?>" ><?php print $item['section']; ?></a></span>
+          <div class="consultation-paragraph-inner"><?php print render($item['field']); ?></div>
+          <?php print render($item['replies']) ?>
+          <?php print render($item['links']) ?>
+        </div>
       </div>
     <?php endforeach; ?>
   </div>
