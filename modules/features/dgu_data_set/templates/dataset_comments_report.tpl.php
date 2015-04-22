bbf297a3d4887b9a6c19249a52836fac24f199fe
Noel Sharpe <noels@radnetwork.co.uk>
2015-04-22 21:11:58 +0100

    Update template to use preferred style for print.

--- modules/features/dgu_data_set/templates/dataset_comments_report.tpl.php
+++ modules/features/dgu_data_set/templates/dataset_comments_report.tpl.php
@@ -1,16 +1,16 @@
 <h1>Dataset comments</h1>
-<h3>Latest comments on all datasets published by <a href="/publisher/<?php print $publisher->name ?>"><?php print($publisher->title); ?></a></h3>
+<h3>Latest comments on all datasets published by <a href="/publisher/<?php print $publisher->name ?>"><?php print $publisher->title ?></a></h3>
 <div class="dataset-comment-report">
 <?php
 foreach  ($dataset_comments as $row):?>
-  <h3>Dataset: <a href="/dataset/<?php print($row->name)?>"><?php print($row->dataset_title)?></a></h3>
+  <h3>Dataset: <a href="/dataset/<?php print $row->name ?>"><?php print $row->dataset_title ?></a></h3>
   <div class="result boxed">
     <div class="comment">
       <div class="reply-header">
-        <h3><?php print($row->subject); ?></h3>
-        <span class=""submitted">Posted by <a href="/user/<?php print($row->uid);?>"><?php print($row->user_name);?></a></span><span> on <?php print($row->post_date);?> </span>
+        <h3><?php print $row->subject ?></h3>
+        <span class=""submitted">Posted by <a href="/user/<?php print $row->uid ?>"><?php print $row->user_name ?></a></span><span> on <?php print $row->post_date ?> </span>
       </div>
-      <p><?php print($row->comment);?></p>
+      <p><?php print $row->comment ?></p>
     </div>
   </div>
 <?php endforeach ?>
